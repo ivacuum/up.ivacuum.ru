918239a4c7041ea3794222c2acfff73eff515e86
Sergey Pankov <vacuum@ivacuum.ru>
2013-03-27 14:30:29 +0400

    Вынесение вызова инициализации фреймворка в отдельный файл

---
+++ bootstrap.php
@@ -0,0 +1,3 @@
+<?php
+
+require(__DIR__ . '/../_/fw/1.2.2/bootstrap.php');
--- public_html/index.php
+++ public_html/index.php
@@ -6,7 +6,7 @@
 
 namespace app;
 
-require('../../_/fw/1.2.2/bootstrap.php');
+require('../bootstrap.php');
 
 /* Маршрутизация запроса */
 $app['router']->_init()->handle_request();
